2ac8e176f7e5dfecf2fbbae3f715f4e15ec00542
KalvinAlAzhaar <87465503+KalvinAlAzhaar@users.noreply.github.com>
2021-08-28 21:42:47 +0700

    Add files via upload

---
+++ web-php/project/content.php
@@ -0,0 +1,13 @@
+<?php 
+
+    $titel = "Blog Belajar PHP SMK - Coding itu mudah!";
+
+    $logo = 'Kalvin Al Azhaar';
+
+    $nav = [
+    'Home'=>'index.php',
+    'About'=>'isi-php/about.php',
+    'Product'=>'isi-php/product.php',
+    'Service'=>'isi-php/service.php',
+    'Contact'=>'isi-php/contact.php'
+];
